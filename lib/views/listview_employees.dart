bc49c739c60758182b3b99a05f5e077569cb8462
NiMo- <mo5-98@hotmail.com>
2020-04-09 18:24:22 +0300

    get all Employee at first run app

--- lib/views/listview_employees.dart
+++ lib/views/listview_employees.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:sqlliteproject/models/employee.dart';
+import 'package:sqlliteproject/utils/database_helper.dart';
 
 class ListViewEmployee extends StatefulWidget {
   @override
@@ -6,6 +9,22 @@ class ListViewEmployee extends StatefulWidget {
 }
 
 class _ListViewEmployeeState extends State<ListViewEmployee> {
+
+  List<Employee> items = List();
+  DatabaseHelper databaseHelper = DatabaseHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    //To get all Employee at first run app
+    databaseHelper.getAllEmployee().then((employees){
+      setState(() {
+        employees.forEach((employee){
+          items.add(Employee.map(employee));
+        });
+      });
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
